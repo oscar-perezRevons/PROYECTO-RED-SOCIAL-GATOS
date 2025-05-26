0fb97a91884ac1ba236003b387cbb5185036f990
ela251643010 <daniela.condori.b@ucb.edu.bo>
2025-05-26 16:52:44 -0400

    creacion paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImagesComponent } from './paginas/images/images.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
