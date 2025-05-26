294fbadefc6ce6672d99c39015017820cf89ac9f
ela251643010 <daniela.condori.b@ucb.edu.bo>
2025-05-26 18:29:53 -0400

    endpoint obtenerTodasLasImagenes

--- src/app/paginas/images/images.component.ts
+++ src/app/paginas/images/images.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { image } from '../../interfaces/image';
+import { GatoService } from '../../servicios/gato.service';
+import { ImageComponent } from "../../elementos/image/image.component";
 
 @Component({
   selector: 'app-images',
-  imports: [],
+  imports: [ImageComponent],
   templateUrl: './images.component.html',
   styleUrl: './images.component.scss'
 })
-export class ImagesComponent {
 
+export class ImagesComponent {
+    listaDeImagenes: image[]=[];
+  imagenService: GatoService = inject(GatoService);
+
+
+  constructor( ){
+  this.imagenService.obtenerTodasLasImagenes().subscribe( 
+      data =>{
+        this.listaDeImagenes=data,
+        console.log("mis datos",data)
+      }, 
+      error=> console.log( error),
+      ()=> console.log('FIN')
+    )
+  }
 }
---
+++ src/app/servicios/gato.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { image } from '../interfaces/image';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GatoService {
+  
+
+  constructor(private http:HttpClient) { }
+  obtenerTodasLasImagenes():Observable<image[]>
+  {
+    return this.http.get<image[]>('https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0')
+  }
+  obtenerImagenPorId(id:string):Observable<image>{
+    return this.http.get<image>('https://api.thecatapi.com/v1/images/BkIEhN3pG'+id)
+
+  }
+}
